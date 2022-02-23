f44fa5ae53efab9bcbbe4c4b162b56e662c283ad
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-23 13:08:05 +0900

    [clean] 開発前の準備

---
+++ registration.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>会員登録画面</title>
+  </head>
+  <body>
+      <h1>会員登録画面</h1>
+  </body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+
+let mix = require('laravel-mix');
+
+mix.js('src/app.js', 'dist').setPublicPath('dist')
+    .browserSync({
+        proxy: {
+            target: "localhost",
+        },
+        files: [
+            "*.*",
+        ],
+        open: false,
+        reloadOnRestart: true,
+    }
+);
