6e0ab818e0b43795405b765565f4fb617f7a1d5b
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-23 23:29:22 +0900

    [fix] ログアウトした時の処理

--- forum.php
+++ forum.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+include_once('./dbconnect.php');
+
+if (isset($_SESSION['id'])) {
+    $id = $_SESSION['id'];
+    $statement = $pdo->prepare('SELECT * FROM members WHERE id=:id');
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $member = $statement->fetch(PDO::FETCH_ASSOC);
+} else {
+    header('Location: ./index.php');
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,10 +21,13 @@
   <body>
       <?php
       // デバッグ
+      print_r($_COOKIE);
+      print_r($_SESSION);
       ?>
       <h1>犬・猫 どちら派掲示板</h1>
+      <p><?php echo $member['email']; ?> さん、ようこそ</p>
+      <p><a href="index.php">ログアウト</a></p>
       <form action="./forum.php">
-          <p>***さん、ようこそ</p>
           <textarea name="text1" id="" cols="30" rows="10"></textarea><br>
           <input type="submit" value="投稿する">
       </form>
