e7bdb1f78d0d7f55e0b527e3113e699986360f27
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-27 16:20:32 +0900

    [fix] registration.php デザイン変更

--- index.php
+++ index.php
@@ -32,34 +32,11 @@ if (!empty($_POST)) {
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <title>犬・猫 どちら派掲示板</title>
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        background-color: #17a2b8;
-        height: 100vh;
-      }
-      #login .container #login-row #login-column #login-box {
-        margin-top: 120px;
-        max-width: 600px;
-        min-height: 320px;
-        border: 1px solid #9C9C9C;
-        background-color: #EAEAEA;
-      }
-      #login .container #login-row #login-column #login-box #login-form {
-        padding: 20px;
-      }
-      #login .container #login-row #login-column #login-box #login-form #register-link {
-        margin-top: -55px;
-        margin-bottom: 23px;
-      }
-      .submit {
-        margin-top: 30px;
-      }
-    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>[TOP] 犬・猫 どちら派掲示板</title>
   </head>
   <body>
     <div id="login">
@@ -95,7 +72,9 @@ if (!empty($_POST)) {
             </div>
         </div>
     </div>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="./dist/app.js"></script>
   </body>
 </html>
--- registration.php
+++ registration.php
@@ -27,10 +27,20 @@ function is_no_error($error) {
     }
 }
 
+function keep_your_input($s) {
+  if (!empty($_POST[$s])) {
+    $_SESSION[$s] = htmlspecialchars($_POST[$s]);
+  }
+}
+
 if (!empty($_POST)) {
     $error['name'] = is_empty_member('name');
 
+    keep_your_input('name');
+
     $error['email'] = is_empty_member('email');
+
+    keep_your_input('email');
 
     if ($error['email'] != 'blank') {
         $error['email'] = email_exists($pdo, $_POST['email']);
@@ -53,42 +63,68 @@ if (!empty($_POST)) {
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico">
-    <title>会員登録画面</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>[会員登録] 犬・猫 どちら派掲示板</title>
   </head>
-  <body>
-      <?php
-      // デバッグ
-      // echo $error['password2'];
-      ?>
-      <h1>会員登録画面</h1>
-      <p><a href="./index.php">TOPへ戻る</a></p>
-      <form action="./registration.php" method="post">
-          <input type="radio" name="animal" id="dog" value="dog" checked>
-          <label for="dog">犬</label>
-          <input type="radio" name="animal" id="cat" value="cat">
-          <label for="cat">猫</label>
-          <?php if($error['name'] == "blank"): ?>
-            <p>名前を入力してください。</p>
-          <?php endif; ?>
-          <p>名前<input type="text" name="name" id=""></p>
-          <?php if($error['email'] == "blank"): ?>
-            <p>emailを入力してください。</p>
-          <?php elseif($error['email'] == "duplicate"): ?>
-            <p>emailが登録されています。</p>
-          <?php endif; ?>
-          <p>email<input type="email" name="email" id=""></p>
-          <?php if($error['password'] == "blank"): ?>
-            <p>パスワードを入力してください。</p>
-          <?php endif; ?>
-          <p>パスワード<input type="password" name="password" id=""></p>
-          <?php if($error['password2'] == "blank"): ?>
-            <p>確認のためのパスワードを入力してください。</p>
-          <?php elseif($error['password2'] == "difference"): ?>
-            <p>パスワードが上記と違います。</p>
-          <?php endif; ?>
-          <p>パスワード再入力<input type="password" name="password2" id=""></p>
-          <input type="submit" value="登録">
-      </form>
+  <body>  
+      <div id="login">
+        <h3 class="text-center text-white pt-5">犬・猫 どちら派掲示板</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="./registration.php" method="post">
+                            <h3 class="text-center text-info">会員登録</h3>
+                            <?php if($error['name'] == "blank"): ?>
+                              <p class="text-danger">Nameを入力してください。</p>
+                            <?php endif; ?>
+                            <div class="form-group">
+                                <label for="name" class="text-info">Name:</label><br>
+                                <input type="text" name="name" id="name" class="form-control" value="<?= $_SESSION['name']; ?>">
+                            </div>
+                            <?php if($error['email'] == "blank"): ?>
+                              <p class="text-danger">Emailを入力してください。</p>
+                            <?php elseif($error['email'] == "duplicate"): ?>
+                              <p class="text-danger">Emailが登録されています。</p>
+                            <?php endif; ?>
+                            <div class="form-group">
+                                <label for="email" class="text-info">Email:</label><br>
+                                <input type="email" name="email" id="email" class="form-control"  value="<?= $_SESSION['email']; ?>">
+                            </div>
+                            <?php if($error['password'] == "blank"): ?>
+                              <p class="text-danger">パスワードを入力してください。</p>
+                            <?php endif; ?>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <?php if($error['password2'] == "blank"): ?>
+                              <p class="text-danger">確認のためのパスワードを入力してください。</p>
+                            <?php elseif($error['password2'] == "difference"): ?>
+                              <p class="text-danger">パスワードが上記と違います。</p>
+                            <?php endif; ?>
+                            <div class="form-group">
+                                <label for="password2" class="text-info">Password 再入力:</label><br>
+                                <input type="text" name="password2" id="password2" class="form-control">
+                            </div>
+                            <div class="form-group submit">
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="確認画面へ">
+                            </div>
+                            <div id="register-link" class="text-right">
+                                <span class="text-info"><a href="./index.php">TOPへ戻る</a></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="./dist/app.js"></script>
   </body>
 </html>
