281413f1801b96695dab51f0beb8f3a5ec10c1d6
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-27 12:11:06 +0900

    [add] index.php ログインボタン修正中

--- index.php
+++ index.php
@@ -33,26 +33,81 @@ if (!empty($_POST)) {
   <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="favicon.ico">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <title>犬・猫 どちら派掲示板</title>
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+        background-color: #17a2b8;
+        height: 100vh;
+      }
+      #login .container #login-row #login-column #login-box {
+        margin-top: 120px;
+        max-width: 600px;
+        height: 320px;
+        border: 1px solid #9C9C9C;
+        background-color: #EAEAEA;
+      }
+      #login .container #login-row #login-column #login-box #login-form {
+        padding: 20px;
+      }
+      #login .container #login-row #login-column #login-box #login-form #register-link {
+        margin-top: -55px;
+      }
+      .submit {
+        margin-top: 30px;
+      }
+    </style>
   </head>
   <body>
-      <?php
-      // デバッグ
-      // print_r($member);
-      // print_r($_COOKIE);
-      // print_r($_SESSION);
-      ?>
-      <h1>犬・猫 どちら派掲示板</h1>
-      <?php if ($error['login'] == 'blank'): ?>
-        <p>メールとパスワードを入力してください。</p>
-      <?php elseif ($error['login'] == 'failed'): ?>
-        <p>メールとパスワードが間違っています。</p>
-      <?php endif ?>
-      <form action="./index.php" method="post">
-          <p>email<input type="email" name="email" id=""></p>
-          <p>パスワード<input type="password" name="password" id=""></p>
-          <input type="submit" value="ログインする">
-      </form>
-      <p>会員登録は<a href="./registration.php">こちら</a></p>
+    <h1>犬・猫 どちら派掲示板</h1>
+    <?php if ($error['login'] == 'blank'): ?>
+      <p>メールとパスワードを入力してください。</p>
+    <?php elseif ($error['login'] == 'failed'): ?>
+      <p>メールとパスワードが間違っています。</p>
+    <?php endif ?>
+    <form action="./index.php" method="post">
+        <p>email<input type="email" name="email" id=""></p>
+        <p>パスワード<input type="password" name="password" id=""></p>
+        <input type="submit" value="ログインする">
+    </form>
+    <p>会員登録は<a href="./registration.php">こちら</a></p>
+
+
+    <div id="login">
+        <h3 class="text-center text-white pt-5">犬・猫 どちら派掲示板</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="./index.php" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <p>メールとパスワードを入力してください。</p>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Email:</label><br>
+                                <input type="email" name="email" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group submit">
+                                <!-- <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br> -->
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="ログイン">
+                            </div>
+                            <div id="register-link" class="text-right">
+                                <span class="text-info">会員登録は<a href="#">こちら</a></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   </body>
 </html>
