25856060a3b7c6959da4302ce6b760c778b801fc
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-27 09:41:10 +0900

    [add] forum.php 画像の投稿、削除機能追加

--- delete.php
+++ delete.php
@@ -9,7 +9,13 @@ if (isset($_SESSION['id'])) {
     $messages->bindValue(':id', $id, PDO::PARAM_INT);
     $messages->execute();
     $message = $messages->fetch(PDO::FETCH_ASSOC);
+
+    // 削除内容が投稿者本人か確認
     if ($message['created_by'] == $_SESSION['id']) {
+        // 画像ファイルの削除
+        unlink('images/' . $message['image']);
+
+        // 投稿内容を削除
         $del = $pdo->prepare('DELETE FROM posts WHERE id=:id');
         $del->bindValue(':id', $id, PDO::PARAM_INT);
         $del->execute();
