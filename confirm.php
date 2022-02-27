c9cf2638b13bf93eaa81853597ff72113ebb3d1c
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-28 00:17:52 +0900

    [clean] sass導入

--- confirm.php
+++ confirm.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['join'])) {
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./dist/app.css">
     <title>[確認] 犬・猫 どちら派掲示板</title>
   </head>
   <body>
